d42f16034f156935c28424fb97e3100e6ccf8715
jenanouar1234 <jenanouar1234@gmail.com>
2024-05-08 14:16:13 +0100

    pushed

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Definition of class Base
+"""
+
+
+class Base:
+    """
+    args:
+        private attribute: __nb_objects(This will be the base)
+        id: default None
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:  # Change comparison to use 'is not None'
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
