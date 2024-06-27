a767ae143208368af8507bf0ea0adb9a28abaf74
jenanouar1234 <jenanouar1234@gmail.com>
2024-06-27 05:10:22 +0100

    pushed

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -9,18 +9,18 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: {} <username> <password> <database>".format(argv[0]))
         exit(1)
-        
+
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'
         .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     state_to_update = session.query(State).filter(State.id == 2).first()
     if state_to_update:
         state_to_update.name = "New Mexico"
         session.commit()
-    
+
     session.close()
